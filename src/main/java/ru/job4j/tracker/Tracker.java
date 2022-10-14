daddfc9bbb68e9ba3ff1bf518cd4a8617a493ac9
UruGwaii2286 <106101910+UruGwaii2286@users.noreply.github.com>
2022-10-14 12:14:06 +0500

    5. Tracker - хранилище [#396 #351267]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -18,15 +18,15 @@ public class Tracker {
     }
 
     public Item[] findByName(String key) {
-        Item[] byName = new Item[items.length];
+        Item[] result = new Item[items.length];
         int num = 0;
         for (int i = 0; i < size; i++) {
             if (items[i].getName().equals(key)) {
-                byName[num] = items[i];
+                result[num] = items[i];
                 num++;
             }
         }
-        return Arrays.copyOf(byName, num);
+        return Arrays.copyOf(result, num);
     }
 
     public Item findById(int id) {
