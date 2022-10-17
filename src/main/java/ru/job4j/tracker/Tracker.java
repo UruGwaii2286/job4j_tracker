796c0f1843792e9e586ae005f9b559f5b4b652d7
UruGwaii2286 <106101910+UruGwaii2286@users.noreply.github.com>
2022-10-17 13:37:20 +0500

    7. Метод удаления заявки Tracker.delete [#211749 #351635]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -67,8 +67,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean result = false;
         if (index != -1) {
-            items[index] = null;
-            System.arraycopy(items, index + 1, items, index, size - index);
+            System.arraycopy(items, index + 1, items, index, size - 1);
             items[size - 1] = null;
             size--;
             result = true;
