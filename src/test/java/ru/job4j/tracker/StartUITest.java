83e8b6daa1f3f83956a91236cbf3aa1ca79a0780
UruGwaii2286 <106101910+UruGwaii2286@users.noreply.github.com>
2022-10-21 20:08:35 +0500

    5. Input и полиморфизм. [#181094]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,13 +1,13 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StartUITest {
     @Test
     public void whenAddItem() {
-        Input input = new ConsoleInput();
+        String[] answers = {"Fix PC"};
+        Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
         StartUI.createItem(input, tracker);
         Item created = tracker.findAll()[0];
