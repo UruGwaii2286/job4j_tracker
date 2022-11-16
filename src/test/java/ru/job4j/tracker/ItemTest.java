48baa9e5b63dd1caf78dcc1bde7d53d1cb920ea8
UruGwaii2286 <106101910+UruGwaii2286@users.noreply.github.com>
2022-11-16 18:46:35 +0500

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import ru.job4j.collection.ItemDescByName;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ItemTest {
+    @Test
+    public void comparableSort() {
+        List<Item> items = Arrays.asList(
+                new Item(1, "A"),
+                new Item(6, "B"),
+                new Item(3, "C")
+        );
+        Collections.sort(items);
+        List<Item> expected = Arrays.asList(
+                new Item(1, "A"),
+                new Item(3, "C"),
+                new Item(6, "B")
+        );
+        assertEquals(expected, items);
+    }
+
+    @Test
+    public void itemAscByName() {
+        List<Item> items = Arrays.asList(
+                new Item(1, "C"),
+                new Item(6, "A"),
+                new Item(3, "B")
+        );
+        items.sort(new ItemAscByName());
+        List<Item> expected = Arrays.asList(
+                new Item(6, "A"),
+                new Item(3, "B"),
+                new Item(1, "C")
+        );
+        assertEquals(expected, items);
+    }
+
+    @Test
+    public void itemDescByName() {
+        List<Item> items = Arrays.asList(
+                new Item(1, "B"),
+                new Item(6, "C"),
+                new Item(3, "A")
+        );
+        items.sort(new ItemDescByName());
+        List<Item> expected = Arrays.asList(
+                new Item(6, "C"),
+                new Item(1, "B"),
+                new Item(3, "A")
+        );
+        assertEquals(expected, items);
+    }
+}
