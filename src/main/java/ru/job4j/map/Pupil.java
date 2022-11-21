8cae4bbffd47cadf485c9f20f9eca0229edc4c30
UruGwaii2286 <106101910+UruGwaii2286@users.noreply.github.com>
2022-11-21 17:07:20 +0500

    1

--- src/main/java/ru/job4j/map/Pupil.java
+++ src/main/java/ru/job4j/map/Pupil.java
@@ -2,5 +2,20 @@ package ru.job4j.map;
 
 import java.util.List;
 
-public record Pupil(String name, List<Subject> subjects) {
+public class Pupil {
+    private String name;
+    private List<Subject> subjects;
+
+    public Pupil(String name, List<Subject> subjects) {
+        this.name = name;
+        this.subjects = subjects;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Subject> getSubjects() {
+        return subjects;
+    }
 }
