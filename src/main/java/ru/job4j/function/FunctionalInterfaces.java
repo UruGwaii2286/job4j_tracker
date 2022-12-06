e36b7cb5dc032d6b6ff204ed78f0f2aeffd0209d
UruGwaii2286 <106101910+UruGwaii2286@users.noreply.github.com>
2022-12-06 11:14:11 +0500

    2.2. Функциональный интерфейс [#247497]

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -9,26 +9,28 @@ import java.util.function.*;
 public class FunctionalInterfaces {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
-        List<String> biConList = List.of("one", "two", "three", "four", "five", "six", "seven");
-        BiConsumer<Integer, String> biCon = (num, str) -> map.put(num, str);
-        int i = 1;
-        for (String str : biConList) {
-            biCon.accept(i, str);
-            i++;
-        }
+        BiConsumer<Integer, String> biCon = (index, str) -> map.put(index, str);
+        biCon.accept(1, "one");
+        biCon.accept(2, "two");
+        biCon.accept(3, "three");
+        biCon.accept(4, "four");
+        biCon.accept(5, "five");
+        biCon.accept(6, "six");
+        biCon.accept(7, "seven");
 
-        BiPredicate<Integer, String> biPred = (num, str) -> num % 2 == 0 || str.length() == 4;
-        for (Integer k : map.keySet()) {
-            System.out.println(biPred.test(k, map.get(k)));
+        BiPredicate<Integer, String> biPred
+                = (index, str) -> index % 2 == 0 || str.length() == 4;
+        for (Integer i : map.keySet()) {
+            if (biPred.test(i, map.get(i))) {
+                System.out.println("key: " + i + " value: " + map.get(i));
+            }
         }
         Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
-        System.out.println(sup.get());
-
-        Consumer<String> con = a -> System.out.println(a);
-        Function<String, String> func = a -> a.toUpperCase();
-        for (String s : map.values()) {
-            con.accept(s);
-            System.out.println(func.apply(s));
+        List<String> strings = sup.get();
+        Consumer<String> con = s -> System.out.println(s);
+        Function<String, String> func = s -> s.toUpperCase();
+        for (String s : strings) {
+            con.accept(func.apply(s));
         }
     }
 }
